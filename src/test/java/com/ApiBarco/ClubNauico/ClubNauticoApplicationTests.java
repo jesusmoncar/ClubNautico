777856abcb5c7a4cd53f36fd60c54c034bdf17b5
Jesus Montero Carrascoso <jesus.monteroc@eviden.com>
2024-05-22 08:56:06 +0200

    subida de proyecto

---
+++ src/test/java/com/ApiBarco/ClubNauico/ClubNauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiBarco.ClubNauico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
