7f55ebc4ce8b2d5a0c3a8088a4ebcb90d15a76e6
CristianRM05 <crm00423@gmail.com>
2024-05-26 00:02:27 +0200

    creado ship controller y ship service

--- src/main/java/com/ApiBarco/Controller/ShipController.java
+++ src/main/java/com/ApiBarco/Controller/ShipController.java
@@ -1,35 +1,37 @@
-//package com.ApiBarco.Controller;
-//
-//import com.ApiBarco.DTO.ShipDTO;
-//
-//import com.ApiBarco.Service.ShipService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/Ships")
-//public class ShipController {
-//
-//    @Autowired
-//    private ShipService shipService;
-//
-//    @GetMapping("{/id}")
-//    public ShipDTO getShipById(@PathVariable long id) {
-//        return shipService.getShipById(id);
-//    }
-//
-//    @GetMapping
-//    public List<ShipDTO> getAllShips() {
-//        return shipService.getAllShip();
-//    }
-//
-//    @PostMapping
-//    public void addShip(@RequestBody ShipDTO shipDTO) {
-//        shipService.createShip(shipDTO);
-//    }
-//
-//
-//
-//}
+package com.ApiBarco.controller;
+
+import com.ApiBarco.DTO.ShipDTO;
+import com.ApiBarco.entity.Ship;
+import com.ApiBarco.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/ships")
+public class ShipController {
+
+    @Autowired
+    private ShipService shipService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShipDTO> getShipById(@PathVariable("id") Long id) {
+        ShipDTO shipDTO = shipService.getShipById(id);
+        return new ResponseEntity<>(shipDTO, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ShipDTO>> getAllShips() {
+        List<ShipDTO> shipDTOs = shipService.getAllShips();
+        return new ResponseEntity<>(shipDTOs, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ship> createShip(@RequestBody ShipDTO shipDTO) {
+        Ship ship = shipService.createShip(shipDTO);
+        return new ResponseEntity<>(ship, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/ApiBarco/Service/ShipService.java
+++ src/main/java/com/ApiBarco/Service/ShipService.java
@@ -1,35 +1,46 @@
-//package com.ApiBarco.service;
-//
-//import com.ApiBarco.DTO.ShipDTO;
-//import com.ApiBarco.entity.Member;
-//import com.ApiBarco.repository.ShipRepository;
-//import com.ApiBarco.entity.Ship;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//public class ShipService {
-//
-//    @Autowired
-//    private ShipRepository shipRepository;
-//
-//    public ShipDTO getShipById( long id){
-//        Ship ship = shipRepository.findById(id).orElseThrow(() -> new RuntimeException("Ship not found"));
-//        return convertToDTO(ship);
-//    }
-//
-//    public List<ShipDTO> getAllShip() {
-//        List<Ship> ships = shipRepository.findAll();
-//        return ships.stream().map(this::convertToDTO).collect(Collectors.toList());
-//    }
-//
-//    public void createShip( ShipDTO shipDTO) {
-//        Ship  ship = new Member(shipDTO.getid_ship(), shipDTO.registration_tag());
-//        shipRepository.save(shipDTO);
-//    }
-//
-//    private ShipDTO convertToDTO(Ship ship){
-//        return new ShipDTO(ship.getId_ship(), ship.getRegistration_tag());
-//    }
-//}
+package com.ApiBarco.service;
+
+import com.ApiBarco.DTO.ShipDTO;
+import com.ApiBarco.entity.Member;
+import com.ApiBarco.entity.Ship;
+import com.ApiBarco.repository.MemberRepository;
+import com.ApiBarco.repository.ShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ShipService {
+
+    @Autowired
+    private ShipRepository shipRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public ShipDTO getShipById(Long id) {
+        Ship ship = shipRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ship not found"));
+        return convertToDTO(ship);
+    }
+
+    public List<ShipDTO> getAllShips() {
+        List<Ship> ships = shipRepository.findAll();
+        return ships.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+
+    public Ship createShip(ShipDTO shipDTO) {
+        Member member = memberRepository.findById(shipDTO.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+        Ship ship = new Ship(shipDTO.getId_ship(), shipDTO.getRegistration_tag(), member);
+        shipRepository.save(ship);
+        return ship;
+    }
+
+    private ShipDTO convertToDTO(Ship ship) {
+        Long memberId = (ship.getMember() != null) ? ship.getMember().getId_member() : null;
+        return new ShipDTO(ship.getId_ship(), ship.getRegistration_tag(), memberId);
+    }
+}
