935bd53b798875185fb92656d374a2093c3e3d9c
jesusmoncar <jesus.montero.jmc@gmail.com>
2024-05-27 19:46:41 +0200

    service master

--- src/main/java/com/ApiBarco/Service/MasterService.java
+++ src/main/java/com/ApiBarco/Service/MasterService.java
@@ -1,9 +1,18 @@
 package com.ApiBarco.Service;
 
 
+import com.ApiBarco.DTO.MasterDTO;
+import com.ApiBarco.Exeption.ClubNauticoNotFoundException;
+import com.ApiBarco.entity.Departures;
+import com.ApiBarco.entity.Master;
+import com.ApiBarco.repository.DepartureRepository;
 import com.ApiBarco.repository.MasterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MasterService {
@@ -12,5 +21,63 @@ public class MasterService {
     private MasterRepository masterRepository;
 
     @Autowired
-    private Departur
+    private DepartureRepository departureRepository;
+
+    public MasterDTO getMasterById(long id) throws ClubNauticoNotFoundException{
+        Optional<Master> masterOpt = masterRepository.findById(id);
+        if(!masterOpt.isPresent()){
+            throw new ClubNauticoNotFoundException("El patron con la id " + id + " no existe");
+        }
+        Master master = masterOpt.get();
+        return convertToDTO(master);
+    }
+
+    public List<MasterDTO> getAllMembers() throws ClubNauticoNotFoundException{
+        List<Master> masters = masterRepository.findAll();
+        return masters.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+
+
+  /*  public Departures createDeparture(DepartureDTO departureDTO) throws ClubNauticoNotFoundException {
+        Master master = masterRepository.findById((long) departureDTO.getMasterId())
+                .orElseThrow(() -> new ClubNauticoNotFoundException("El patrón con la id " + departureDTO.getMasterId() + " no existe"));
+
+        Departures departure = new Departures(departureDTO.getDeparture_time(), master);
+        return departuresRepository.save(departure);
+    }*/
+
+    public Master createMaster(MasterDTO masterDTO){
+        List<Departures> departures = masterDTO.getDepartureIds().stream()
+                .map(departureId ->{
+                    try{
+                        return departureRepository.findById(departureId)
+                                .orElseThrow() -> new ClubNauticoNotFoundException("La salida con la id " + departureId + " no existe"));
+                    }catch(ClubNauticoNotFoundException e){
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collec(Collectors.toList());
+    Master master = new Master(masterDTO.getId_master(), masterDTO.getName(),masterDTO.getLast_name(), masterDTO.getPermit_number());
+    departures.forEach(departure -> departure.setMaster(master));
+
+    return masterRepository.save(master);
+
+    }
+
+    public void deleteAllMasters(){
+        masterRepository.deleteAll();
+    }
+
+    public void deleteMasterById(long masterId) throws ClubNauticoNotFoundException{
+        Optional<Master> masterOpt = masterRepository.findById(masterId);
+        if(!masterOpt.isPresent()){
+            throw new ClubNauticoNotFoundException("El patron con la id " + masterId + " no existe");
+        }
+        masterRepository.deleteById(masterId);
+    }
+
+    private MasterDTO convertToDTO(Master master){
+        return new MasterDTO(master.getId_master(), master.getName(), master.getLast_name(), master.getPermit_number(), departureIds);
+    }
+
 }
