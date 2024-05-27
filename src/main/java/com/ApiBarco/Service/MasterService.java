fb9f60910c24154a890029d85a3969b6c558a321
Jesus Montero Carrascoso <jesus.monteroc@eviden.com>
2024-05-27 18:27:39 +0200

    reposiroty master

---
+++ src/main/java/com/ApiBarco/Service/MasterService.java
@@ -0,0 +1,16 @@
+package com.ApiBarco.Service;
+
+
+import com.ApiBarco.repository.MasterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MasterService {
+
+    @Autowired
+    private MasterRepository masterRepository;
+
+    @Autowired
+    private Departur
+}
---
+++ src/main/java/com/ApiBarco/repository/MasterRepository.java
@@ -0,0 +1,8 @@
+package com.ApiBarco.repository;
+
+
+import com.ApiBarco.entity.Master;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MasterRepository extends JpaRepository<Master, Long> {
+}
