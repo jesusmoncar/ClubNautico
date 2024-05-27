f0bb36a46e0dc07513bd2560d568eafef4b1f3a0
CristianRM05 <crm00423@gmail.com>
2024-05-27 18:27:24 +0200

    agregado repo y service

---
+++ src/main/java/com/ApiBarco/Service/DepartureService.java
@@ -0,0 +1,13 @@
+package com.ApiBarco.Service;
+
+import com.ApiBarco.repository.DepartureRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartureService {
+    private DepartureRepository departureRepository;
+
+    public DepartureService() {
+
+    }
+}
